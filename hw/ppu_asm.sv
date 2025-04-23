641fc07f3d672325392dcd53e092745fb1a3f5a0
ShinyMiraidon <bind-naps0r@icloud.com>
2025-04-23 17:31:06 -0400

    Chore: Added buffer comments to ppu_asm.sv

--- hw/ppu_asm.sv
+++ hw/ppu_asm.sv
@@ -8,6 +8,7 @@ module PPU_asm(
     input logic [10:0] hcount,
     input logic [9:0] vcount,
     input logic vblank,
+    input logic hsync,
 
     output logic rw_tile_buffer, rw_tile_graphics, rw_sprite_graphics, rw_color_palettes, rw_OAM,
     output logic [31:0] write_data_tile_buffer, write_data_tile_graphics, write_data_sprite_graphics, write_data_OAM,
@@ -26,13 +27,18 @@ module PPU_asm(
 
     //Once per line
     logic [1279:0] background_line_buffer;
-    logic [7:0] [6:0] sprite_on_line_buffer;
+    logic [6:0] [6:0] sprite_on_line_buffer;
 
     //Once per frame
-    logic [7:0] [15:0] sprite_x_buffer;
-    logic [7:0] [15:0] sprite_y_buffer;
+    logic [6:0] [15:0] sprite_x_buffer;
+    logic [6:0] [15:0] sprite_y_buffer;
     logic [127:0] sprite_palette_buffer;
-    logic [7:0] [23:0] color_palette_buffer;
+    logic [6:0] [23:0] color_palette_buffer;
+
+    //Vblank memory access pointers
+    logic [6:0] coords_sprite_load;
+    logic [6:0] palette_sprite_load;
+    logic [2:0] palette_ram_pointer;
 
 
     always @(posedge clk) begin
@@ -46,15 +52,34 @@ module PPU_asm(
             rw_OAM <= 0;
             prev_hcount <= 0;
             prev_vcount <= 0;
+            coords_sprite_load <= 0;
+            palette_sprite_load <= 0;
+            palette_ram_pointer <= 0;
         end
 
         //set buffers that fill once per frame
         else if (vblank) begin
+            
+            //fill sprite_x_buffer
+
+            //fill sprite_y_buffer
+
+            //fill sprite_palette_buffer
+
+            //fill color_palette_buffer
 
         end
 
-        
+        else if (hsync) begin
+            
 
+        end
+        else begin
+            //Reset vblank memory pointers
+            coords_sprite_load <= 0;
+            palette_sprite_load <= 0;
+            palette_ram_pointer <= 0;
+        end
     end
 
 

17f70153c67c7799ccfaf9acbfc941ae4fa83ee6
ShinyMiraidon <bind-naps0r@icloud.com>
2025-04-24 16:56:08 -0400

    Feat: First draft of Vblank buffer fills

--- hw/ppu_asm.sv
+++ hw/ppu_asm.sv
@@ -27,19 +27,24 @@ module PPU_asm(
 
     //Once per line
     logic [1279:0] background_line_buffer;
-    logic [6:0] [6:0] sprite_on_line_buffer;
+    logic [2:0] [31:0] sprite_graphics_buffer;
+    logic [2:0] [6:0] sprites_on_line;
 
     //Once per frame
+    logic [2:0] [23:0] color_palette_buffer;
     logic [6:0] [15:0] sprite_x_buffer;
     logic [6:0] [15:0] sprite_y_buffer;
-    logic [127:0] sprite_palette_buffer;
-    logic [6:0] [23:0] color_palette_buffer;
+    logic [6:0] sprite_palette_buffer;
+    logic [6:0] [6:0] sprite_tile_id_buffer;
+    logic [6:0] [1:0] sprite_rotation_buffer;
 
     //Vblank memory access pointers
-    logic [6:0] coords_sprite_load;
-    logic [6:0] palette_sprite_load;
-    logic [2:0] palette_ram_pointer;
+    logic [7:0] cords_sprite_load;
+    logic [7:0] palette_sprite_load;
+    logic [3:0] palette_ram_pointer;
+    
 
+    //Hsync memory access pointers
 
     always @(posedge clk) begin
         
@@ -55,16 +60,84 @@ module PPU_asm(
             coords_sprite_load <= 0;
             palette_sprite_load <= 0;
             palette_ram_pointer <= 0;
+
         end
 
         //set buffers that fill once per frame
         else if (vblank) begin
-            
-            //fill sprite_x_buffer and sprite_y_buffer
-
-            //fill sprite_palette_buffer
 
             //fill color_palette_buffer
+            if (palette_ram_pointer == 0) begin
+                rw_color_palettes <= 0;
+                addr_color_palettes <= palette_ram_pointer;
+                palette_ram_pointer <= palette_ram_pointer + 1;
+            end
+
+            else if (palette_ram_pointer < 8) begin
+                rw_color_palettes <= 0;
+                addr_color_palettes <= palette_ram_pointer;
+                color_palette_buffer[palette_ram_point - 1] = read_data_color_palettes;
+                palette_ram_pointer <= palette_ram_pointer + 1;
+            end
+
+            else if (palette_ram_point = 8) begin
+                rw_color_palettes <= 0;
+                addr_color_palettes <= 0;
+                color_palette_buffer[palette_ram_point - 1] = read_data_color_palettes;
+                palette_ram_pointer <= palette_ram_pointer + 1;
+            end
+
+            else begin
+                rw_color_palettes <= 0;
+                addr_color_palettes <= 0;
+            end
+            
+            //fill sprite_x_buffer and sprite_y_buffer
+            if (cords_sprite_load == 0) begin
+                rw_OAM <= 0;
+                addr_OAM <= cords_sprite_load * 2 + 1;
+                cords_sprite_load <= cords_sprite_load + 1;
+            end
+
+            else if (cords_sprite_load < 128) begin
+                rw_OAM <= 0;
+                addr_OAM <= cords_sprite_load * 2 + 1;
+                sprite_x_buffer[cords_sprite_load - 1] <= read_data_OAM[15:0];
+                sprite_y_buffer[cords_sprite_load - 1] <= read_data_OAM[31:16];
+                cords_sprite_load <= cords_sprite_load + 1;
+            end
+
+            else if (cords_sprite_load == 128) begin
+                rw_OAM <= 0;
+                addr_OAM <= palette_sprite_load;
+                sprite_x_buffer[cords_sprite_load - 1] <= read_data_OAM[15:0];
+                sprite_y_buffer[cords_sprite_load - 1] <= read_data_OAM[31:16];
+                cords_sprite_load <= cords_sprite_load + 1;
+                palette_sprite_load <= palette_sprite_load + 1;
+            end
+
+            else if (palette_sprite_load < 128) begin
+                rw_OAM <= 0;
+                addr_OAM <= palette_sprite_load * 2;
+                sprite_palette_buffer[palette_sprite_load - 1] <= read_data_OAM[7];
+                sprite_tile_id_buffer[palette_sprite_load - 1] <= read_data_OAM[6:0];
+                sprite_rotation_buffer[palette_sprite_load - 1] <= read_data_OAM[31:30];
+                palette_sprite_load <= palette_sprite_load + 1;
+            end
+
+            else if (palette_sprite_load == 128) begin
+                rw_OAM <= 0;
+                addr_OAM <= 0;
+                sprite_palette_buffer[palette_sprite_load - 1] <= read_data_OAM[7];
+                sprite_tile_id_buffer[palette_sprite_load - 1] <= read_data_OAM[6:0];
+                sprite_rotation_buffer[palette_sprite_load - 1] <= read_data_OAM[31:30];
+                palette_sprite_load <= palette_sprite_load + 1;
+            end
+            
+            else begin
+                rw_OAM <= 0;
+                addr_OAM <= 0;
+            end
 
         end
 
@@ -77,6 +150,9 @@ module PPU_asm(
             coords_sprite_load <= 0;
             palette_sprite_load <= 0;
             palette_ram_pointer <= 0;
+
+
+
         end
     end
 

8c0d575a3ba1cdc02ae6ba5999d149ac30a6c6e0
ShinyMiraidon <bind-naps0r@icloud.com>
2025-05-01 17:21:47 -0400

    Chore: PPU_TOP

--- hw/ppu_top.sv
+++ hw/ppu_top.sv
@@ -12,77 +12,8 @@ module ppu_top(
     output logic VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n, VGA_SYNC_n
 );
 
-    PPU_asm asm (
-       
-    );
-
-    tile_buffer tile_buffer_mem (
-        .clk(),
-        .reset(),
-        .rw(),
-        .write_data(),
-        .addr(),
-        .read_data()
-    );
-
-    tile_graphics tile_graphics_mem (
-        .clk(),
-        .reset(),
-        .rw(),
-        .write_data(),
-        .addr(),
-        .read_data()
-    );
-
-    sprite_graphics sprite_graphics_mem (
-        .clk(),
-        .reset(),
-        .rw(),
-        .write_data(),
-        .addr(),
-        .read_data()
-    );
-
-    color_palettes color_palettes_mem (
-        .clk(),
-        .reset(),
-        .rw(),
-        .write_data(),
-        .addr(),
-        .read_data()
-    );
-
-    OAM oam_mem (
-        .clk(),
-        .reset(),
-        .rw(),
-        .write_data(),
-        .addr(),
-        .read_data()
-    );
-
-    combined_priority_encoder priority_encoder (
-        .pixel_data_in(),
-        .palette_data_in(),
-        .pixel_data_out(),
-        .palette_data_out()
-    );
-
-    shift_register_block shift_regs (
-        .load_data(),
-        .enable(),
-        .clk(),
-        .reset(),
-        .load(),
-        .out_data()
-    );
-
-    vga vga_cont (
-        .clk(),
-        .reset(),
-        .pixel_color(),
-        .
-    );
+    
+   
 
 
 

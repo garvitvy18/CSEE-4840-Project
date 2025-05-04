f305a8d367aba83e9e00654badf5a1b34678529d
ShinyMiraidon <bind-naps0r@icloud.com>
2025-05-04 16:35:38 -0400

    Fix: ends

--- hw/ppu_asm.sv
+++ hw/ppu_asm.sv
@@ -489,8 +489,6 @@ module PPU_asm(
 
             end
             else shift_load_background <= 0;
-
-        end 
             
             //Logic to handle pixel doubling
             if (hcount[0]) begin
